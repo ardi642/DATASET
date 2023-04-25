701503e6419037eedf1b5ad14d14132e9bbd1039
Ardiansyah Latif <ardiansyahlatif6420@gmail.com>
2023-04-26 00:37:13 +0700

    menghubungkan browser melalui wsEndpoint

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import puppeteer from "puppeteer-core";
+import dotenv from "dotenv";
+const wsEndpoint = dotenv.config().parsed.WS_ENDPOINT;
 const browser = await puppeteer.connect({
-  browserWSEndpoint: 'ws://127.0.0.1:9555/devtools/browser/671ec4bb-6ad3-4c9d-8e2c-0d6e0fc236bd'
+  browserWSEndpoint: wsEndpoint
 });
-global.browser = browser;
-// const page = await browser.newPage();
+
+const page = await browser.newPage();
